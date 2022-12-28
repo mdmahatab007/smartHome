524b49dff37981ebe06c8d7036fc42334f651036
mdmahatab007 <116806523+mdmahatab007@users.noreply.github.com>
2022-12-28 12:13:28 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,38 +22,82 @@ $(window).resize(function(){
 	}
 });
 
+
+/*=-=-=-=-=-=-=-=-
+    logInPage
+=-=-=-=-=-=-=-=-= */
+
+$('#userName').on('click',function(){
+  $('#usrLabel').animate({
+    top:'-8%'
+  });
+});
+
+$('#userPass').on('click',function(){
+  $('#passLabel').animate({
+    top:'-8%'
+  });
+});
+
+$('.logInPageItems > i').on('click',function(){
+  $('#logInPage').fadeOut();
+});
+
+
+
+
+
+
 }) (jQuery);
 
 
-// wow js
-var wow = new WOW(
-    {
-      boxClass:     'wow',      // animated element css class (default is wow)
-      animateClass: 'animate__animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       true,       // trigger animations on mobile devices (default is true)
-      live:         true,       // act on asynchronously loaded content (default is true)
-      callback:     function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      },
-      scrollContainer: null,    // optional scroll container selector, otherwise use window,
-      resetAnimation: true,     // reset animation on end (default is true)
-    }
-  );
-  wow.init();
-
-
-//preLoader
-var time = 5;
-setInterval( function() {
+/*=-=-=-=-=-=-=-=-
+    preLoader
+=-=-=-=-=-=-=-=-= */
+  var time = 5;
+  setInterval( function() {
   var loader = document.getElementById('preLoader');
   var content = document.getElementById('smartHomeContent');
-  time -= 1;
+   time -= 1;
   if(time<0){
     loader.style.display = 'none';
     content.style.opacity = 1;
-    return;
+     return;
+   }
+}, 1000); 
+
+/*=-=-=-=-=-=-=-=-
+    logInPage
+=-=-=-=-=-=-=-=-= */
+  var logPage = 30;
+  setInterval( function() {
+    logPage--;
+    var logInPage = document.getElementById('logInPage');
+    if( logPage <= 0 && logPage >= -3 ){
+      logInPage.style.display = 'block';
+      return
+    }
+  }, 1000);
+
+/*=-=-=-=-=-=-=-=-
+    wow js
+=-=-=-=-=-=-=-=-= */
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animate__animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
   }
-  console.log(time)
-}, 1000);
+);
+wow.init();
+
+
+
